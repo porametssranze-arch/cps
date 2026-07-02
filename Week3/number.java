89aa758c118b6996ce1fbc6d7e9ea04efc2bb973
porametssranze-arch <porametssranze@gmail.com>
2026-07-02 16:06:23 +0700

    add number.java

---
+++ Week3/number.java
@@ -0,0 +1,11 @@
+void main (){
+   IO.print("Enter number1 of : ");
+       int number1 = Integer.parseInt(IO.readln());
+
+        IO.print("Enter number2 of   : ");
+        int number2 = Integer.parseInt(IO.readln());
+
+        int total = number1 + number2;
+
+        IO.println("Total number of number1 and number2 = " + total + " quantity");
+}
