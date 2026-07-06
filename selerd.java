22258237c70729488084e2fce1768bacc0a825a8
PorametSiladang <porametssranze@gmail.com>
2026-07-06 16:49:41 +0700

    Delete selerd.java

--- selerd.java
+++
@@ -1,8 +0,0 @@
-void main() {
-    IO.print("Enter (THB) : ");
-    double selerd = Double.parseDouble(IO.readln());
-
-    double tex = selerd * 0.10;
-
-    IO.println("tex = %.2f ".formatted(tex));
-}
