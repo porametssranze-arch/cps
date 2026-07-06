79e423acf6bceb405517652d9d6870249210911e
PorametSiladang <porametssranze@gmail.com>
2026-07-06 16:49:10 +0700

    Delete bml.java

--- bml.java
+++
@@ -1,9 +0,0 @@
-void main() {
-    IO.print("Enter (kg) : ");
-    double weight = Double.parseDouble(IO.readln());
-    IO.print("Enter (km : ");
-    double height = Double.parseDouble(IO.readln());
-    double bmi = weight / (height * height);
-
-    IO.println("Weight = %.2f kg, Height = %.2f m, BMI = %.2f".formatted(weight, height, bmi));
-}
