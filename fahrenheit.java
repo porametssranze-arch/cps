f674d83a23a384ae9039cf772f5852762cf8620f
PorametSiladang <porametssranze@gmail.com>
2026-07-06 16:49:22 +0700

    Delete fahrenheit.java

--- fahrenheit.java
+++
@@ -1,8 +0,0 @@
-void main() {
-    IO.print("Enter (f) : ");
-    double fahrenheit = Double.parseDouble(IO.readln());
-
-    double celsius = (5.0 / 9) * (fahrenheit - 32);
-
-    IO.println("Celsius = %.2f ".formatted(celsius));
-}
